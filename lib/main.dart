ed056355c7c8de9950f26b5b2f0bfeeb32fa08b4
sleduap <sleduap@gmail.com>
2021-11-08 13:04:56 +0545

    yes

--- lib/main.dart
+++ lib/main.dart
@@ -93,7 +93,7 @@ class _LicenceState extends State<Licence> {
               //   height: 5,
               // ),
               Text(
-                "$licenceType asd",
+                "$licenceType asde",
                 style: const TextStyle(
                   fontSize: 24,
                   // fontWeight: FontWeight.bold,
